48857108727a026b00172afdd46709b49d9395da
Benjamin Peña Olvera <benjamin.pena.olvera@gmail.com>
2018-09-06 19:43:11 -0500

    Very first commit

---
+++ Projects.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Projects extends Component {
+  render(){
+    return(
+      <div>
+        <h1>Projects</h1>
+
+        <p>
+          Projects section
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Projects;
---
+++ We.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class We extends Component {
+  render(){
+    return(
+      <div>
+        <h1>Who we are?</h1>
+
+        <p>
+          Who we are section section
+        </p>
+      </div>
+    );
+  }
+}
+
+export default We;
